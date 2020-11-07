c7ed670169352458377ed187ed865863d17eecb3
M0u1ea5 <74062509+M0u1ea5@users.noreply.github.com>
2020-11-07 09:15:08 +0530

    Add files via upload

---
+++ SPACE INVADERS.py
@@ -0,0 +1,157 @@
+import pygame,sys
+import random
+import math
+
+
+#initialize the pygame
+pygame.init()
+
+
+#displaying screen with size
+screen=pygame.display.set_mode((1000,600))
+
+
+#display title 
+pygame.display.set_caption('SPACE INVADERS')
+icon=pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
+
+#player
+player_image=pygame.image.load('spaceship.png')
+playerX=480
+playerY=480
+playerXchange=0
+
+#enemy
+enemy_image=[]
+enemyX=[]
+enemyY=[]
+enemyXchange=[]
+enemyYchange=[]
+num_enemies=100
+for i in range(num_enemies):
+    enemy_image.append(pygame.image.load('alien.png'))
+    enemyX.append(random.randint(50,900))
+    enemyY.append(random.randint(50,80))
+    enemyXchange.append(0.4)
+    enemyYchange.append(30)
+
+#bullet
+bullet_image=pygame.image.load('bullet.png')
+bulletX=0
+bulletY=480
+bulletYchange=2
+bullet_state='ready'
+
+#score
+score_value=0
+font=pygame.font.Font('freesansbold.ttf',32)
+
+textX=8
+textY=8
+
+#gameover
+over=pygame.font.Font('freesansbold.ttf',64)
+
+def game_over():
+    over_text=font.render('GAME OVER',True,(255,255,255))
+    screen.blit(over_text,(400,300))
+    
+def show_score(x,y):
+    score=font.render('Score:'+ str(score_value),True,(255,255,255))
+    screen.blit(score,(x,y))
+
+def player(x,y):
+    screen.blit(player_image,(x,y))
+
+def enemy(x,y,i):
+    screen.blit(enemy_image[i],(x,y))
+
+def fire_bullet(x,y):
+    global bullet_state
+    bullet_state='fire'
+    screen.blit(bullet_image,(x+20,y+10))
+
+def iscollision(enemyX,enemyY,bulletX,bulletY):
+    distance = math.sqrt(math.pow(enemyX-bulletX,2)+ math.pow(enemyY-bulletY,2))
+    if distance < 27:
+        return True
+    else:
+        return False
+        
+
+#game loop
+while True:
+    screen.fill((0,0,0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+        if event.type == pygame.KEYDOWN:
+            
+            if event.key == pygame.K_LEFT:
+                playerXchange = -1
+            if event.key == pygame.K_RIGHT:
+                playerXchange = +1
+            if event.key == pygame.K_SPACE:
+                if bullet_state == 'ready':
+                    bulletX = playerX
+                    fire_bullet(bulletX,bulletY)
+                
+        
+        if event.type == pygame.KEYUP:
+            playerXchange=0
+
+    #player boundary
+    playerX += playerXchange
+
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 936:
+        playerX = 936
+        
+    #enemy boundary
+    for i in range(num_enemies):
+        if enemyY[i] > 400:
+            for j in range(num_enemies):
+                enemyY[j]=2000
+            game_over()
+            break
+        enemyX[i] += enemyXchange[i]
+        if enemyX[i] <= 0:
+            enemyXchange[i]=1
+            enemyY[i] += enemyYchange[i]
+        elif enemyX[i] >= 936:
+            enemyXchange[i]= -1
+            enemyY[i] +=enemyYchange[i]
+        
+
+
+
+        #collision
+        collision=iscollision(enemyX[i],enemyY[i],bulletX,bulletY)
+        if collision:
+            bulletY=480
+            bullet_state='ready'
+            score_value += 10
+            enemyX[i]=random.randint(50,900)
+            enemyY[i]=random.randint(50,80)
+
+
+        enemy(enemyX[i],enemyY[i],i)
+
+    #bullet bondary
+    if bulletY <=0:
+        bulletY = 480
+        bullet_state ='ready'
+    if bullet_state == 'fire':
+        fire_bullet(bulletX,bulletY)
+        bulletY -= bulletYchange
+
+        
+        
+    
+    player(playerX,playerY)
+    show_score(textX,textY)  
+    pygame.display.update()
+        
---
+++ tic-tac-toe.py
@@ -0,0 +1,169 @@
+import pygame,sys
+import numpy 
+
+pygame.init()
+
+WIDTH=600
+HEIGHT=600
+BG_COLOR=(0,0,0)
+LINE_COLOR=(0,228,235)
+SQUARE_SIZE=200
+LINE_WIDTH=15
+BOARD_ROW=3
+BOARD_COL=3
+CROSS_COLOR=(242,242,242)
+CIRCLE_COLOR=(242,242,242)
+CROSS_WIDTH=25
+
+
+screen=pygame.display.set_mode((WIDTH,HEIGHT))
+screen.fill(BG_COLOR)
+#title and icon
+title=pygame.display.set_caption('TIC-TAC-TOE')
+icon=pygame.image.load('tic-tac-toe.png')
+pygame.display.set_icon(icon)
+
+#vertical and horizontal lines
+def draw_lines():
+    pygame.draw.line(screen,LINE_COLOR,(0,200),(600,200),5)
+    pygame.draw.line(screen,LINE_COLOR,(0,400),(600,400),5)
+    pygame.draw.line(screen,LINE_COLOR,(200,0),(200,600),5)
+    pygame.draw.line(screen,LINE_COLOR,(400,0),(400,600),5)
+
+
+def draw_symbol():
+    for row in range(BOARD_ROW):
+        for col in range(BOARD_COL):
+            if board[row][col]==1:
+                pygame.draw.circle(screen,CIRCLE_COLOR,(int(col*200+100),int(row*200+100)),60,15)
+            elif board[row][col]==2:
+                pygame.draw.line(screen,CROSS_COLOR,(int(col*200+55),int(row*200+200-55)),(int(col*200+200-55),int(row*200+55)),CROSS_WIDTH)
+                pygame.draw.line(screen,CROSS_COLOR,(int(col*200+55),int(row*200+55)),(int(col*200+200-55),int(row*200+200-55)),CROSS_WIDTH)
+
+#numpy
+board=numpy.zeros((BOARD_ROW,BOARD_COL))
+
+
+#marking the square
+def mark_square(row,col,player):
+    board[row][col]=player
+
+#available square
+def available_square(row,col):
+    return board[row][col]==0
+
+#is square full
+def square_full():
+    for row in range(BOARD_ROW):
+        for col in range(BOARD_COL):
+            if board[row][col]==0:
+                return False
+            
+    return True
+
+def game_win(player):
+    #vertical win
+    for col in range(BOARD_COL):
+        if board[0][col]==player and board[1][col]==player and board[2][col]==player:
+            vertical_line(col,player)
+            return True
+    #horizontal win
+    for row in range(BOARD_ROW):
+        if board[row][0]==player and board[row][1]==player and board[row][2]==player:
+            horizontal_line(row,player)
+            return True
+    #diagonal wins
+    if board[2][0]==player and board[1][1]==player and board[0][2]==player:
+        backslash_line(player)
+        return True
+    if board[0][0]==player and board[1][1]==player and board[2][2]==player:
+        frontslash_line(player)
+        return True
+
+    return False
+
+
+
+def vertical_line(col,player):
+    posX=col*SQUARE_SIZE+SQUARE_SIZE//2
+    if player==1:
+        color=CIRCLE_COLOR
+    elif player==2:
+        color=CROSS_COLOR
+
+    pygame.draw.line(screen,color,(posX,15),(posX,HEIGHT-15),15)
+
+def horizontal_win(row,player):
+    posY=row*200+100
+    if player==1:
+        color=CIRCLE_COLOR
+    elif player==2:
+        color=CROSS_COLOR
+
+    pygame.draw.line(screen,color,(15,posY),(WIDTH-15,posY),15)
+
+
+
+def backslash_line(player):
+    if player==1:
+        color=CIRCLE_COLOR
+    elif player==2:
+        color=CROSS_COLOR
+
+
+    pygame.draw.line(screen,color,(15,HEIGHT-15),(WIDTH-15,15),15)
+
+def frontslash_line(player):
+    if player==1:
+        color=CIRCLE_COLOR
+    elif player==2:
+        color=CROSS_COLOR
+
+    pygame.draw.line(screen,color,(15,15),(WIDTH-15,HEIGHT-15),15)
+
+    
+        
+#reset
+def reset():
+    screen.fill(BG_COLOR)
+    draw_lines()
+    player=1
+    for row in range(BOARD_ROW):
+        for col in range(BOARD_COL):
+            board[row][col]==0
+    GAMEOVER=False
+            
+#gameloop
+player=1
+GAMEOVER=False
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+   
+
+        if event.type==pygame.MOUSEBUTTONDOWN and not GAMEOVER:
+            mouseX=event.pos[0]
+            mouseY=event.pos[1]
+
+            #clicked
+            clicked_row=int(mouseY//200)
+            clicked_col=int(mouseX//200)
+            if available_square(clicked_row,clicked_col):
+                mark_square(clicked_row,clicked_col,player)
+                if game_win(player):
+                    GAMEOVER=True
+                player=player%2+1
+                
+
+                draw_symbol()
+
+        if event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_SPACE:
+                reset()
+
+
+    draw_lines()
+    pygame.display.update()
+
+        
